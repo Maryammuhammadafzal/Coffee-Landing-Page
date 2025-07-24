f4078ed9afea40a5c7a201f705f3d03bee00b64a
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-24 06:14:31 +0500

    craeting hero section

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,9 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 const HeroPage = () => {
   return (
-    <div className='w-full '>
-      
+    <div className='w-full h-auto flex justify-center items-center py-6 px-8'>
+      <div className='w-ful min-h-screen'>
+        <Image src='/images/hero-image.png' alt='image' width={200} height={200} className='w-full h-full' />
+      </div>
     </div>
   )
 }
