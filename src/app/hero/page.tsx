5fca119507eb5243212870aacbc439541b3d8d8c
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-24 06:08:19 +0500

    added fonts and colours

---
+++ src/app/hero/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroPage = () => {
+  return (
+    <div className='w-full '>
+      
+    </div>
+  )
+}
+
+export default HeroPage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Catamaran, Castoro_Titling } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const catamaran = Catamaran({
+  variable: "--font-catamaran",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const castoroTitling = Castoro_Titling({
+  variable: "--font-castoro-titling",
   subsets: ["latin"],
+  weight: ['400']
 });
 
 export const metadata: Metadata = {
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${catamaran.variable} ${castoroTitling.variable} antialiased`}
       >
         {children}
       </body>
