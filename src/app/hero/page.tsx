305e5b1b8454ad74cca0d4e94e1ce7603b77c33e
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-24 11:29:46 +0500

    hero section creaed

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import TestimonialCarousel from '@/components/TestimonialCarousel'
 import { Button } from '@/components/ui/button'
+import { ArrowUpRight } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -30,12 +31,24 @@ const HeroPage = () => {
 
         </div>
         <div className='w-auto h-auto flex gap-6'>
-          <div className='max-w-xs w-xs h-auto'>
+          <div className='max-w-xs relative w-xs h-auto'>
             <Image src='/images/vector-1.png' alt='image' width={300} height={200} className='w-full h-full' />
+            <div className='w-full h-full flex absolute top-0 left-0'>
+              <div className='content w-full h-full p-2 text-background flex flex-col gap-3 relative'>
+                <div className='arrow w-[107px] h-[107px] flex justify-center items-center absolute top-0 right-0 rounded-full bg-background text-'>
+                  <Image src='/images/up-right-icon.png' alt="icon" width={30} height={30} />
+                </div>
+
+                <h2 className='font-mono text-3xl pt-5 pl-4 max-w-[200px]'>Visit Our Café</h2>
+                <p className=' text-lg leading-8 px-4'>Address: [123 Brew Street, Your City] <br />
+                  Hours: Mon–Fri: 7am–7pm | Sat–Sun: 8am–8pm <br />
+                  Phone: [Your Phone Number]</p>
+              </div>
+            </div>
           </div>
 
           <div className='w-md h-auto bg-primary rounded-[400px] flex justify-center items-center'>
-            <TestimonialCarousel/>
+            <TestimonialCarousel />
           </div>
 
 
